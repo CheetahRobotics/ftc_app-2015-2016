caa7b8f7fec50918ea33c11d67582b8c8018311b
Robert Woodley <blwoodley@yahoo.com>
2016-01-03 11:56:38 -0600

    BeaconSeeker:X and Y were swapped.  Put in tones to indicate whether yellow circle is left or right of center. Goes silent on no yellow circle.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BeaconSeekerOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BeaconSeekerOp.java
@@ -1,5 +1,7 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.util.Log;
 
 import com.qualcomm.ftcrobotcontroller.BeaconSeekerActivity;
@@ -8,14 +10,21 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class BeaconSeekerOp extends PushBotTelemetry {
     private static final String TAG = "BeaconSeekerOp";
-
+    private static ToneGenerator toneGen1;
     @Override
     public void init() {
+        Log.i("test", "in init()");
+
         BeaconSeekerActivity.enableBeaconSeeker();
+        this.toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+        this.toneGen1.startTone(ToneGenerator.TONE_SUP_CONFIRM,150);
+        this.toneGen1.startTone(ToneGenerator.TONE_DTMF_0,150);
+        this.toneGen1.startTone(ToneGenerator.TONE_DTMF_A,150);
     }
 
     @Override
     public void start() {
+        Log.i("test", "in start()");
 
     }
 
@@ -31,6 +40,23 @@ public class BeaconSeekerOp extends PushBotTelemetry {
         telemetry.addData("Beacon X,Y (Percent)", ((int) (100 * BeaconSeekerActivity.mBeaconCenterPointPercent.x)) + "%, " +
                 ((int) (100 * BeaconSeekerActivity.mBeaconCenterPointPercent.y)) + "%");
 
+        if (count%50 == 0) {
+            if (BeaconSeekerActivity.mBeaconCenterPointPixels.x > 0 && BeaconSeekerActivity.mBeaconCenterPointPixels.y > 0) {
+
+                if (BeaconSeekerActivity.mBeaconCenterPointPercent.y > 0.5) {
+                    this.toneGen1.startTone(ToneGenerator.TONE_PROP_BEEP, 150);
+                    telemetry.addData("Beacon Y (Percent)", " > 50 ");
+                    //set_drive_power(-0.5f, 0.5f);
+                } else {
+                    this.toneGen1.startTone(ToneGenerator.TONE_DTMF_A, 150);
+                    telemetry.addData("Beacon Y (Percent)", " < 50 ");
+                    //set_drive_power(0.5f, -0.5f);
+                }
+            }
+        }
+        telemetry.addData("State = ", v_state);
+
+
         switch (v_state) {
             case 0:
                 Log.i("test", "case 0");
@@ -38,10 +64,9 @@ public class BeaconSeekerOp extends PushBotTelemetry {
 
                 break;
             case 1:
-                Log.i("test", "case 1");
                 run_using_encoders();
 
-                set_drive_power(0.5f, 0.5f);
+                set_drive_power(0.0f, 0.00f);
 
                 if (have_drive_encoders_reached(16501, 16501)) {
                     set_drive_power(0.0f, 0.0f);
@@ -49,17 +74,7 @@ public class BeaconSeekerOp extends PushBotTelemetry {
                 }
                 break;
             case 2:
-                Log.i("test", "case 2");
                 run_using_encoders();
-
-                if (BeaconSeekerActivity.mBeaconCenterPointPercent.x > 0.5) {
-                    telemetry.addData("Beacon X (Percent)", " > 50 right rightMotor = hardwareMap.dcMotor.get(\"right_drive\");");
-                    set_drive_power(-0.5f, 0.5f);
-                }
-                else {
-                    telemetry.addData("Beacon X (Percent)", " < 50 left leftMotor = hardwareMap.dcMotor.get(\"left_drive\");");
-                    set_drive_power(0.5f, -0.5f);
-                }
 
                 if (has_right_drive_encoder_reached(4880)) {
                     set_drive_power(0.0f, 0.0f);
